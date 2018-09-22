5afe214ffdb8419cb37b7a486c9938b526baf32b
Umesh Kumar Rajendran <umeshrajan2014@gmail.com>
2018-09-22 19:10:04 +0530

    update

---
+++ Anugraha/View/Anu_Stock_InHand.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace Anugraha.View
+{
+    partial class Anu_Stock_InHand
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Anu_Stock_InHand
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Anu_Stock_InHand";
+            this.Size = new System.Drawing.Size(920, 670);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
